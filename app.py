840cb372e1c332d3078d41d30894f622ad294115
Fabian <fabian.chavannes@students.ffhs.ch>
2024-05-01 12:10:41 +0200

    initial setting

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+quotes = [
+    {"id": 1, "quote": "Quote 1", "author": "Author 1"},
+    {"id": 2, "quote": "Quote 2", "author": "Author 2"}
+]
+
+@app.route('/')
+def index():
+    return "Welcome to the Random Quote API!"
+
+@app.route('/quote')
+def get_quote():
+    return jsonify(random.choice(quotes))
+
+if __name__ == '__main__':
+    app.run()
