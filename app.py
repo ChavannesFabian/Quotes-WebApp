5e345bcd7dd7b51d46da346c0116d5f5a6e5b310
Fabian <fabian.chavannes@students.ffhs.ch>
2024-05-01 12:42:13 +0200

    TestApp

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
-from flask import Flask, jsonify
-import random
+import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-quotes = [
-    {"id": 1, "quote": "Quote 1", "author": "Author 1"},
-    {"id": 2, "quote": "Quote 2", "author": "Author 2"}
-]
 
 @app.route('/')
 def index():
-    return "Welcome to the Random Quote API!"
+   print('Request for index page received')
+   return render_template('index.html')
 
-@app.route('/quote')
-def get_quote():
-    return jsonify(random.choice(quotes))
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
